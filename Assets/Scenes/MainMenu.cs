98877fe6a04068d29026d67890d2b48c822a4fb7
Georg <demolitionok@gmail.com>
2020-08-02 16:42:45 +0300

    Add basic MainMenu functionallity

---
+++ Assets/Scenes/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject settingPanel;
+    public void StartGame() 
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void EnableSettingsPanel()
+    {
+        settingPanel.SetActive(true);
+    }
+    public void DisableSettingsPanel()
+    {
+        settingPanel.SetActive(false);
+    }
+    public void ExitGame() 
+    {
+        Debug.Log("Quitting");
+        Application.Quit();
+    }
+}
