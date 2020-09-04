1ffaabce9e9bc3a4945252c035e6e0068dd58f43
Georg <demolitionok@gmail.com>
2020-09-04 22:28:38 +0300

    Add events to enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,54 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
-    public float health;
+    private float _health;
+    public float health
+    {
+        set
+        {
+            _health = value;
+            OnHpChange.Invoke();
+
+            if (_health > maxHealth)
+                _health = maxHealth;
+            if (health <= 0)
+            {
+                OnDeath.Invoke();
+                Destroy(gameObject);
+            }
+        }
+        get
+        {
+            return _health;
+        }
+    }
+    public UnityEvent OnHpChange;
+    public UnityEvent OnDeath;
     public float maxHealth;
 
-    public GameObject healthBar;
     public Slider healthBarSlider;
 
     public GameObject lootDrop;
-    
+
     void Start()
     {
+        OnHpChange.AddListener(SetHealthValue);
         health = maxHealth;
-        healthBarSlider = healthBar.GetComponent<Slider>();
+        OnDeath.AddListener(SpawnLootDrop);
     }
     public void DealDamage(float damage) 
     {
-        healthBar.SetActive(true);
         health -= damage;
-        CheckDeath();
-        healthBarSlider.value = CalculateHealthPercentage();
     }
     public void HealCharacter(float heal)
     {
         health += heal;
-        CheckOverheal();
+    }
+    private void SpawnLootDrop()
+    {
+        Instantiate(lootDrop, transform.position, Quaternion.identity);
+    }
+    public void SetHealthValue() 
+    {
         healthBarSlider.value = CalculateHealthPercentage();
     }
-    private void CheckDeath() 
-    {
-        if (health <= 0)
-        {
-            Instantiate(lootDrop, transform.position, Quaternion.identity);
-            Destroy(gameObject);
-        }
-    }
-    private void CheckOverheal() 
-    {
-        if (health > maxHealth) 
-        {
-            health = maxHealth;
-        }
-    }
-
-    private float CalculateHealthPercentage() 
+    private float CalculateHealthPercentage()
     {
         return (health / maxHealth);
     }
+
 }
---
+++ Assets/Scripts/UI Scripts/HealthSlider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSlider : MonoBehaviour
+{
+    Slider healthBarSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBarSlider = gameObject.GetComponent<Slider>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
